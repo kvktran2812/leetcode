a8583f75d43c2dd579cec45f9f0beb91d27b4b02
kvktran2812 <kvktran@gmail.com>
2024-08-07 17:31:14 +0700

    fibonacci with dp

--- problems/dp.py
+++ problems/dp.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Set
 
-def limbing(steps: int) -> int:
+def climbing(steps: int) -> int:
     if steps == 0:
         return 0
     if steps == 1:
@@ -16,3 +16,20 @@ def limbing(steps: int) -> int:
     for i in range(3, steps+1):
         L[i] = L[i-1] + L[i-2]
     return L[-1]
+
+def fibonacci(n: int) -> int:
+    if n == 0:
+            return 0
+    if n == 1:
+        return 1
+    fib = [0] * (n + 1)
+    fib[0] = 0
+    fib[1] = 1
+
+    i = 2
+    while (i <= n):
+        fib[i] = fib[i-1] + fib[i-2]
+        i += 1
+
+    return fib[n]
+
