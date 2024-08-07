090187733938fd56834fe5a4b22858418eae74ad
kvktran2812 <kvktran@gmail.com>
2024-08-08 01:18:22 +0700

    solve count bits

--- problems/dp.py
+++ problems/dp.py
@@ -47,3 +47,19 @@ def longest_common_subsequence(X: str, Y: str) -> int:
                 dp[i][j] = max(dp[i][j-1], dp[i-1][j])
     
     return dp[m][n] 
+
+
+def count_bits(n: int) -> List[int]:
+    if n == 0:
+        return [0]
+    
+    ans = [0] * (n + 1)
+    ans[0] = 0
+    ans[1] = 1
+    
+    for i in range(2, n + 1):
+        if i % 2 == 0:
+            ans[i] = ans[i // 2]
+        if i % 2 == 1:
+            ans[i] = ans[i // 2] + 1
+    return ans
