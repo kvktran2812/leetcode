9dc3944b9c05e0b1b2ac5a772964bef13fe832b2
kvktran2812 <kvktran@gmail.com>
2024-08-07 17:32:02 +0700

    longest common subsequence

--- problems/dp.py
+++ problems/dp.py
@@ -33,3 +33,17 @@ def fibonacci(n: int) -> int:
 
     return fib[n]
 
+def longest_common_subsequence(X: str, Y: str) -> int:
+    m = len(X)
+    n = len(Y)
+
+    dp = [[0] * (n + 1) for _ in range(m+1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[m - i] == Y[n - j]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+    
+    return dp[m][n] 
